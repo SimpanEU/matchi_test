f0c6e7396f44d5045ee57db14bcb80ba844202c1
SimpanEU <simon__n@hotmail.com>
2018-08-13 10:50:30 +0200

    update

--- src/matchi/MATCHi.java
+++ src/matchi/MATCHi.java
@@ -1,9 +1,27 @@
 package matchi;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
 public class MATCHi {
 
-	public MATCHi() {
-		// TODO Auto-generated constructor stub
+	private WebDriver webDriver;
+
+	public void MATCHi() {
+		System.setProperty("webdriver.chrome.driver", "C:/Users/garyh/Desktop/selenium-java-3.8.1/chromedriver.exe");
+		WebElement element = webDriver.findElement(By.xpath(""));
+		element.sendKeys("res1");
+		WebElement element2 = webDriver.findElement(By.id("learn"));
+		element2.click();
+
 	}
 
 }
