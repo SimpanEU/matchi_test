a671b1dfac36ff47c2969f380af5a950e930f243
Simp <Simp@Ss.lan>
2018-08-13 23:14:09 +0200

    a

---
+++ src/matchi/Matchi.java
@@ -0,0 +1,31 @@
+package matchi;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Matchi {
+
+	private WebDriver webDriver;
+
+	public Matchi() {
+		String user = System.getProperty("user.name");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/" + user + "/eclipse-workspace/chromedriver.exe");
+		webDriver = new ChromeDriver();
+		webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		webDriver.manage().window();
+	}
+	
+	public void login() {
+		webDriver.get("http://beta1.matchi.se");
+		
+	}
+	public void logout() {
+		webDriver.get("http://beta1.matchi.se");
+		
+	}
+
+}
---
+++ src/matchi/MatchiTest.java
@@ -0,0 +1,15 @@
+package matchi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MatchiTest {
+
+	@Test
+	public void test() {
+		Matchi matchi = new Matchi();
+		matchi.login();
+	}
+}
