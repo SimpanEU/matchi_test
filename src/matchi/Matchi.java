6f759a94e16cef64d8c3d5291db88b52dcfd451f
Simp <Simp@Ss.lan>
2018-08-21 03:03:12 +0200

    updatos

--- src/matchi/Matchi.java
+++ src/matchi/Matchi.java
@@ -18,15 +18,11 @@ public class Matchi {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/" + user + "/eclipse-workspace/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-<<<<<<< HEAD
 		driver.manage().window();
 	    baseUrl = "https://beta1.matchi.se/";
-=======
->>>>>>> branch 'master' of http://github.com/simpaneu/matchi_test
 	}
 	
 	public void login(String user, String pass) {
-<<<<<<< HEAD
 		driver.get(baseUrl + "/");
 	    driver.findElement(By.linkText("Logga in")).click();
 	    driver.findElement(By.id("username")).clear();
@@ -35,42 +31,9 @@ public class Matchi {
 	    driver.findElement(By.id("password")).sendKeys(pass);
 	    driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
-=======
-		driver.get(baseUrl);
-	    driver.findElement(By.linkText("Logga in")).click();
-	    driver.findElement(By.id("username")).clear();
-	    driver.findElement(By.id("username")).sendKeys(user);
-	    driver.findElement(By.id("password")).clear();
-	    driver.findElement(By.id("password")).sendKeys(pass);
-	    driver.findElement(By.xpath("//button[@type='submit']")).click();
-	}
-	public void search(String name) {
-	    driver.findElement(By.id("q")).clear();
-	    driver.findElement(By.id("q")).sendKeys(name);
-	    driver.findElement(By.name("submit")).click();
-	    
-	}
-	public void booking() {
-		driver.get("http://beta1.matchi.se");
-	}
-	public void payment() {
-		driver.get("http://beta1.matchi.se");
->>>>>>> branch 'master' of http://github.com/simpaneu/matchi_test
 	}
 	public void logout() {
-<<<<<<< HEAD
 	    driver.findElement(By.linkText("Mjuk")).click();
-=======
-	    driver.findElement(By.xpath("//img[contains(@src,'https://beta1.matchi.se//images/avatar_default.png')]")).click();
->>>>>>> branch 'master' of http://github.com/simpaneu/matchi_test
 	    driver.findElement(By.linkText("Logga ut")).click();
 	}
-<<<<<<< HEAD
 
-
-=======
-	public void quit() {
-	driver.quit();
-	}
->>>>>>> branch 'master' of http://github.com/simpaneu/matchi_test
-}
--- src/matchi/MatchiTest.java
+++ src/matchi/MatchiTest.java
@@ -10,17 +10,14 @@ public class MatchiTest {
 	@Test
 	public void test() {
 		Matchi matchi = new Matchi();
-<<<<<<< HEAD
 		
 		matchi.login("mjukvarutestare@mailinator.com", "mjukvarutestare");
 		matchi.logout();
-=======
 		matchi.login("mjukvarutestare@mailinator.com", "mjukvarutestare");
 		matchi.search("Hönö");
 		matchi.booking();
 		matchi.payment();
 		matchi.logout();
 		matchi.quit();
->>>>>>> branch 'master' of http://github.com/simpaneu/matchi_test
 	}
 }
